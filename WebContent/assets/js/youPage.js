572223c7c8705aa992be58330349685efb941bff
DongJae92 <dongjae2379@gmail.com>
2023-04-26 13:47:18 +0900

    dd

--- WebContent/assets/js/youPage.js
+++ WebContent/assets/js/youPage.js
@@ -204,11 +204,11 @@ function findPost(keyword) {
 function addUserInformation(result) {
 
 
-	let search = '';
+	let find = '';
 
 	result.forEach(information => {
 	if(information.userProfileImageSystemName==null){
-					search += `
+					find += `
 			<div class="search-man">
 				<div class="man-left">
 					<a href="#"> <img
@@ -218,7 +218,7 @@ function addUserInformation(result) {
 				</div>`
 			
 		}else{
-		search += `
+		find += `
 			<div class="search-man">
 				<div class="man-left">
 					<a href="#"> <img
@@ -229,7 +229,7 @@ function addUserInformation(result) {
 			}	
 				
 		if(information.userNumber == userNumber ){
-			search +=`<div class="man-right">
+			find +=`<div class="man-right">
 					<div class="man-id">
 						<a href="/mypage/mypageOk.my?userNumber=${information.userNumber}"> ${information.userNickName} </a>
 						
@@ -242,7 +242,7 @@ function addUserInformation(result) {
 				</div>`
 			
 		}	else{	
-			search+=`	<div class="man-right">
+			find+=`	<div class="man-right">
 					<div class="man-id">
 						<a href="/user/youPage.us?userNumber=${information.userNumber}"> ${information.userNickName} </a>
 						
@@ -256,7 +256,7 @@ function addUserInformation(result) {
 		}
 	});
 
-	$('.recent-searches-box').html(search);
+	$('.recent-searches-box').html(find);
 }
 
 //회원 & 게시물 검색
@@ -530,139 +530,6 @@ function addFollowingInfo(result) {
 }
 
 
-//사이드바
-// 여기서부터
-// 검색창
-let $search = $(".find2");
-$search.click(() => {
-	
 
-	$(".sub-find").toggleClass("sub-find__close");
-	$(".sub-find2").removeClass("sub-find2__close");
-	// $(".sub-find").stop().animate({ left: "toggle" });
-});
-
-$(function() {
-	$(".left_sub_menu").hide();
-	$(".has_sub").click(function() {
-		$(".left_sub_menu").fadeToggle(300);
-	});
-	// 왼쪽메뉴 드롭다운
-	$(".sub_menu ul.small_menu").hide();
-	$(".sub_menu ul.big_menu").click(function() {
-		$("ul", this).slideToggle(300);
-	});
-	// 외부 클릭 시 좌측 사이드 메뉴 숨기기
-	$(".overlay").on("click", function() {
-		$(".left_sub_menu").fadeOut();
-		$(".hide_sidemenu").fadeIn();
-	});
-});
-
-// 알림창
-let $search2 = $(".alarmm");
-$search2.click(() => {
-	console.log("click");
-
-	$(".sub-find2").toggleClass("sub-find2__close");
-	$(".sub-find").removeClass("sub-find__close");
-	// $(".sub-find").stop().animate({ left: "toggle" });
-});
-
-$(function() {
-	$(".left_sub_menu").hide();
-	$(".has_sub").click(function() {
-		$(".left_sub_menu").fadeToggle(300);
-	});
-	// 왼쪽메뉴 드롭다운
-	$(".sub_menu ul.small_menu").hide();
-	$(".sub_menu ul.big_menu").click(function() {
-		$("ul", this).slideToggle(300);
-	});
-	// 외부 클릭 시 좌측 사이드 메뉴 숨기기
-	$(".overlay").on("click", function() {
-		$(".left_sub_menu").fadeOut();
-		$(".hide_sidemenu").fadeIn();
-	});
-});
-
-// 여기까지
-
-// ================================================검색 Ajax==================================================== //
-
-$('.search-btn').on('click', function() {
-	let searchCate = $('.drop-down').val();
-
-	if (searchCate == 'user') {
-		$.ajax({
-			url: '/mainAjaxOk.m',
-			type: 'get',
-			data: { input: $('.find-btn').val().trim() },
-			dataType: 'json',
-			success: function(result) {
-				console.log(result);
-				addUserInformation(result);
-				//$('.find-btn').val('');
-			},
-			error: function(a, b, c) {
-				console.log(c);
-			}
-		});
-	} else {
-		findPost($('.find-btn').val().trim());
-	}
-});
-// ================================================검색 Ajax==================================================== //
-
-function findPost(keyword) {
-	let form = document.createElement('form');
-	form.setAttribute('charset', 'utf-8');
-	form.setAttribute('method', 'get');
-	form.setAttribute('action', '/mainSearch.m');
-
-	var hiddenField = document.createElement("input");
-	hiddenField.setAttribute("type", "hidden");
-	hiddenField.setAttribute("name", "keyword");
-	hiddenField.setAttribute("value", keyword);
-	form.appendChild(hiddenField);
-
-	document.body.appendChild(form);
-	form.submit();
-}
-
-
-
-
-
-function addUserInformation(result) {
-
-
-	let text = '';
-console.log(result);
-	result.forEach(info => {
-		text += `
-			<div class="search-man">
-				<div class="man-left">
-					<a href="#"> <img
-						src="/upload/${info.userProfileImageSystemName}" alt="${info.userProfileImageSystemName}"
-						class="man-img" />
-					</a>
-				</div>
-				<div class="man-right">
-					<div class="man-id">
-						<a href="/user/youPage.us?userNumber=${info.userNumber}"> ${info.userNickName} </a>
-						<!-- <a href="#" class="man-id2">hot_boy</a> -->
-					</div>
-					<div class="man-name">
-						${info.userName}
-					</div>
-				</div>
-				
-			</div>
-		`;
-	});
-
-	$('.recent-searches-box').html(text);
-}
 
 
